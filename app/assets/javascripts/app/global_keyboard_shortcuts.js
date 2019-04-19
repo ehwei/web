37984a5f0c5eaf31a85ad0ed8242f2e5ebc95065
Gwyn Morfey <mail@gwynmorfey.com>
2019-04-19 17:07:26 +0100

    Also use CTRL-F as search shortcut

--- app/assets/javascripts/app/global_keyboard_shortcuts.js
+++ app/assets/javascripts/app/global_keyboard_shortcuts.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
     document.body.onkeydown = (e) => {
-        if (e.keyCode == 192) { //§ = jump to search box
+        if (e.keyCode == 192 || (e.keyCode == 70 && e.ctrlKey)) { //§ or CTRL-F = jump to search box
           document.getElementById("search-bar").focus();
           e.stopPropagation();
           e.preventDefault();  
