fb5243e77571fd5935068737a4f0da39652ed8c9
gwyn <gwyn@thoughtbot.com>
2019-09-19 06:58:01 +0100

    Only CTRL-F to search

--- app/assets/javascripts/app/global_keyboard_shortcuts.js
+++ app/assets/javascripts/app/global_keyboard_shortcuts.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
     document.body.onkeydown = (e) => {
-        if (e.keyCode == 192 || (e.keyCode == 70 && e.ctrlKey)) { //§ or CTRL-F = jump to search box
+        if  (e.keyCode == 70 && e.ctrlKey) { //CTRL-F = jump to search box. Not a collision because search-inside-current-note isn't implemented anyway :(
           document.getElementById("search-bar").focus();
           e.stopPropagation();
           e.preventDefault();  
